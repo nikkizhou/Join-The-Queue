04ab6ef83b2476f31e03c77c5666cb4c10af6526
nikkizhou <nikkizhou99@gmail.com>
2022-07-19 23:09:44 +0200

    yeah

--- server/changeStream.js
+++ server/changeStream.js
@@ -1,34 +1,34 @@
-// const simulateAsyncPause = () =>
-//   new Promise(resolve => {
-//     setTimeout(() => resolve(), 1000);
-//   });
+const simulateAsyncPause = () =>
+  new Promise(resolve => {
+    setTimeout(() => resolve(), 1000);
+  });
 
 
-// export default async function runChangeStream(database,mongoClient) {
-//   let changeStream;
-//   try {
-//     const ticketsCollection = await database.collection('tickets')
-//     // open a Change Stream on the "haikus" collection
-//     changeStream = ticketsCollection.watch();
-//     // set up a listener when change events are emitted
-//     changeStream.on("change", next => {
-//       console.log("received a change to the tickets collection: \t", next.fullDocument);
-//     });
+export default async function runChangeStream(database,mongoClient) {
+  let changeStream;
+  try {
+    const ticketsCollection = await database.collection('tickets')
+    // open a Change Stream on the "haikus" collection
+    changeStream = ticketsCollection.watch();
+    // set up a listener when change events are emitted
+    changeStream.on("change", next => {
+      console.log("received a change to the tickets collection: \t", next.fullDocument);
+    });
 
-//     await simulateAsyncPause();
+    await simulateAsyncPause();
 
-//     await ticketsCollection.insertOne({
-//       title: "Record of a Shriveled Datum",
-//       content: "No bytes, no problem. Just insert a document, in MongoDB",
-//     });
+    await ticketsCollection.insertOne({
+      title: "Record of a Shriveled Datum",
+      content: "No bytes, no problem. Just insert a document, in MongoDB",
+    });
     
-//     await simulateAsyncPause();
+    await simulateAsyncPause();
 
-//     await changeStream.close();
-//     console.log("closed the change stream");
+    await changeStream.close();
+    console.log("closed the change stream");
     
-//   } 
-//   finally {
-//     await mongoClient.close();
-//   }
-// }
+  } 
+  finally {
+    await mongoClient.close();
+  }
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,79 +3,121 @@ import express from "express"
 import { connectToDatabase } from "../mongodb.js";
 // import runChangeStream from '../changeStream.js'
 var router = express.Router();
+const {database}  = await connectToDatabase();
 
-const {database,mongoClient}  = await connectToDatabase();
-const business = await database.collection('restaurants').find({}).toArray();
-const tickets = await database.collection('tickets').find({}).toArray();
+const dbQuery = async ()=>{
+  const {database}  = await connectToDatabase();
+  const business = await database.collection('restaurants').find({}).toArray();
+  const tickets = await database.collection('tickets').find({}).toArray();
+  return {business,tickets}
+}
 
 // runChangeStream(database,mongoClient).catch(console.dir);
-
-
-const nextBusinessId = business.length+1;
-console.log(nextBusinessId,'nextBusinessId in backend!');
-const nextTicketId = tickets.length+1;
-console.log(nextTicketId,'nextBusinessId  in backend!');
-
-
-const getTickets = (req,res) => {
-    if (tickets) return res.status(200).send(tickets)
+const getTickets = async (req,res) => {
+    const tickets = await dbQuery()
+    const ticketsDB = tickets.tickets
+    //database.collection('tickets').remove({})
+    if (tickets) return res.status(200).send(ticketsDB)
     return res.status(404).json({ success: false, msg: `Can't find tickets` });
 }
 
-const getTicketsById = (req,res) => {
+const getTicketsById = async (req,res) => {
+    const tickets = await dbQuery()
+    const ticketsDB = tickets.tickets
     const { id } = req.params;
-    const singleTicket = id => tickets.find(ticket => ticket.ticketId == id);
+    const singleTicket = id => ticketsDB.find(ticket => ticket.ticketId == id);
     if (singleTicket(id)) return res.status(200).json(singleTicket(id));
     return res.status(404).json({ success: false, msg: `no ticket with id:${id}` });
 }
 
-const addOneTicket = (req,res)=>{
-    const newTicket = {...req.body, ticketId: nextTicketId};
-    if (req.body) database.collection('tickets').insertOne(newTicket);
+const getTicketsByBusinessId = async (req,res) => {
+    const tickets = await dbQuery()
+    const ticketDB = tickets.tickets
+    const {id}  = req.params;
+
+    const allTickets = id => ticketDB.filter(ticket => ticket.resId == id);
+    if (allTickets(id))  return res.status(200).json(allTickets(id));
+    return res.status(404).json({ success: false, msg: `No busines with id:${id}` });
+}
+
+const addOneTicket = async (req,res)=>{
+    const {database}  = await connectToDatabase();
+    const tickets = await dbQuery();
+    const ticketId = tickets.tickets.length+1
+    const newTicket = {...req.body, ticketId};
+    if (req.body)  await database.collection('tickets').insertOne(newTicket)
     res.status(200).json(newTicket)
 }
 
-const getBusiness = (req,res) => {
-    //console.log(business);
-    if (business) return res.status(200).send(business)
+const deleteTicket = async (req,res)=>{
+    const {database}  = await connectToDatabase();
+    const {id}  = req.params;
+    //console.log(id,'id in deleteTicket');
+    await database.collection('tickets').deleteOne({ticketId:Number(id)})
+    res.status(200).json({message:`ticket with id ${id} document deleted`})
+}
+
+const getBusiness = async (req,res) => {
+    const business = await dbQuery()
+    const businessDB = business.business
+    if (business) return res.status(200).json(businessDB)
     return res.status(404).json({ success: false, msg: `Can't find business` });
 }
 
-const getBusinessByName = (req,res) => {
+const getBusinessByName = async (req,res) => {
+    const business = await dbQuery()
+    const businessDB = business.business
     const name  = req.params.name.toLowerCase();
-    const singleBusiness = name => business.find(busines => busines.name.toLowerCase() === name);
+    const singleBusiness = name => businessDB.find(busines => busines.name.toLowerCase() === name);
     if (singleBusiness(name))  return res.status(200).json(singleBusiness(name));
     return res.status(404).json({ success: false, msg: `No busines with name:${name}` });
 }
 
-const getBusinessById = (req,res) => {
+const getBusinessById = async (req,res) => {
+    const business = await dbQuery()
+    const businessDB = business.business
     const {id}  = req.params;
-    const singleBusiness = id => business.find(business => business.id == id);
+    const singleBusiness = id => businessDB.find(business => business.id == id);
     if (singleBusiness(id))  return res.status(200).json(singleBusiness(id));
     return res.status(404).json({ success: false, msg: `No busines with id:${id}` });
 }
 
-const getTicketsByBusinessId = (req,res) => {
-    const {id}  = req.params;
-    const singleTicket = id => tickets.filter(ticket => ticket.resId == id);
-    if (singleTicket(id))  return res.status(200).json(singleTicket(id));
-    return res.status(404).json({ success: false, msg: `No busines with id:${id}` });
+const addOneBusiness = async (req,res)=>{
+    const {database}  = await connectToDatabase();
+    const business = await dbQuery()
+    const businessId = business.business.length+1
+    if (req.body) {
+      const newBusiness = {...req.body, id:businessId}
+      database.collection('restaurants').insertOne(newBusiness);
+      res.status(200).json(newBusiness)
+    }
 }
 
-const addOneBusiness = (req,res)=>{
-    const newBusiness = req.body;
-    //res.send(newBusiness);
-    if (newBusiness) {
-      database.collection('restaurants').insertOne({...newBusiness, id:nextBusinessId});
-      console.log({...newBusiness, id:nextBusinessId},'');
-      res.status(200).json({success: true, msg: `Business added to database!` })
-    }
+const updateTicketStatusInDB = async (id,status)=>{
+    const {database}  = await connectToDatabase();
+    const filter = {ticketId:id}
+    //console.log(filter)
+    const updateDoc = {$set: {status: status}};
+    return database.collection('tickets').findOneAndUpdate(filter,updateDoc,{'returnNewDocument' : true })
+    // database.collection('tickets').update({"ticketId": id},{$set: {"status": status}})
+}
+
+const updateTicketStatus = async (req,res)=>{
+    const { id } = req.params;
+    const { status } = req.body;
+    const result = await updateTicketStatusInDB(Number(id),status)
+    const tickets = await dbQuery()
+    const ticketDB = tickets.tickets
+    const targetTicket = ticketDB.find(t=>t.ticketId == id)
+    return res.status(200).json(targetTicket);
 }
 
 router.get('/tickets', getTickets)
 router.get('/tickets/business/:id',getTicketsByBusinessId)
 router.get('/tickets/:id', getTicketsById)
 router.post('/tickets', addOneTicket)
+router.put('/tickets/:id', updateTicketStatus)
+router.delete('/tickets/:id', deleteTicket)
 
 router.get('/business', getBusiness)
 router.get('/business/name/:name', getBusinessByName)
