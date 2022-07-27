c45512349f9c0d603ac627ac2975a2234a15fb68
nikkizhou <nikkizhou99@gmail.com>
2022-07-27 22:41:55 +0200

    g

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import { fileURLToPath } from 'url';
 const app = express()
 const PORT = process.env.PORT || 5001
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json({ extended: false }));
 app.use('/api', router);
 
