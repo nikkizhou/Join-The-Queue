da345954f60ee091d096266ac939a43ea76651c1
unknown <nikkizhou99@gmail.com>
2022-12-23 00:22:18 +0100

    ready

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV == 'production') {
   //     res.sendFile(path.join(__dirname, 'static', 'index.html'));
   // });
   app.use(express.static('client/build'));
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, './client', 'build', 'index.html')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
 // Serve the server
